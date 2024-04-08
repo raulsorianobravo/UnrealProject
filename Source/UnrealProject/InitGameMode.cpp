5b61fc107c681219a3dacca46281f6ee97da9e9b
raulsb <raul.soriano@nunegal.com>
2024-04-08 11:10:44 +0200

    [REFRESH]

--- Source/UnrealProject/InitGameMode.cpp
+++ Source/UnrealProject/InitGameMode.cpp
@@ -3,24 +3,11 @@
 // Establece valores predeterminados
 AInitGameMode::AInitGameMode()
 {
-	// Establezca esta character para llamar. Marca() cada fotograma. Puede desactivar esta opción para mejorar el rendimiento si no lo necesita.
-	PrimaryActorTick.bCanEverTick = true;
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClass(TEXT("/Game/Blueprints/BP_PlayerCharacter"));
+	if (PlayerPawnClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnClass.Class;
+	}
 }
 
-// Se llama cuando se inicia el juego o cuando se genera
-void AInitGameMode::BeginPlay()
-{
-	Super::BeginPlay();
-}
 
-// Llamada a cada fotograma
-void AInitGameMode::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
-
-// Se llama para enlazar la funcionalidad a la entrada
-void AInitGameMode::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-}
--- Source/UnrealProject/InitGameMode.h
+++ Source/UnrealProject/InitGameMode.h
@@ -14,14 +14,4 @@ public:
 	// Establece los valores predeterminados para las propiedades de este character
 	AInitGameMode();
 
-protected:
-	// Se llama cuando se inicia el juego o cuando se genera
-	virtual void BeginPlay() override;
-
-public:
-	// Llamada a cada fotograma
-	virtual void Tick(float DeltaTime) override;
-
-	// Se llama para enlazar la funcionalidad a la entrada
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
