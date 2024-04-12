c51d2c3e32ee32018c9f5f96114ccffb6b82592a
raulsb <raul.soriano@nunegal.com>
2024-04-12 11:24:02 +0200

    [ADD] Add Projectile class

---
+++ Intermediate/ProjectFiles/ProjectileActor.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileActor.generated.h"
+
+UCLASS()
+
+class UNREALPROJECT_API AProjectileActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AProjectileActor();
+
+};
+
